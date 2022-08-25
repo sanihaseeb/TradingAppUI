c25927de507f250a5f902f1da453aac547ab8ed6
sanihaseeb <sanihaseeb@hotmail.com>
2022-08-25 18:35:37 +0000

    Working version

--- src/UserPortfolio.js
+++ src/UserPortfolio.js
@@ -9,32 +9,7 @@ import { Doughnut } from 'react-chartjs-2';
 import {Chart, ArcElement} from 'chart.js'
 Chart.register(ArcElement);
 
-export const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Transactions',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+
 
 function UserPortfolio() {
 
@@ -138,6 +113,32 @@ function UserPortfolio() {
 
       return null;
     }
+    let sells = 0;
+    let buys = 0;
+    for(let i = 0; i < history.length; i++) {
+        if(history[i].type === "BUY") buys++;
+        else sells++;
+    }
+
+     const data = {
+        labels: ['BUY', 'SELL'],
+        datasets: [
+          {
+            label: '# of Transactions',
+            data: [buys, sells],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+            
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
   
     
   return (
